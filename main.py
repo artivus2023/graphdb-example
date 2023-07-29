03878abd64cca0ad45f61ed463e7613620c1a43d
Shannon Sands <shannon.sands.1979@gmail.com>
2023-07-29 17:47:26 +1000

    Basic chat repl working

--- main.py
+++ main.py
@@ -1,29 +1,54 @@
 from ctransformers import AutoModelForCausalLM
 import tqdm
+from collections import deque
 
-llm = AutoModelForCausalLM.from_pretrained('models/nous-hermes-llama-2-7b.ggmlv3.q2_K.bin', model_type='llama')
+# Initialize the model
+llm = AutoModelForCausalLM.from_pretrained('models/nous-hermes-llama-2-7b.ggmlv3.q2_K.bin', model_type='llama', stream=True)
+
+# Initialize the conversation history
+history = deque(maxlen=10)
+
+MAX_TOKENS = 2048
 
 system_prompt = """Below is an instruction that describes a task. Write a response that appropriately completes the request.
 
 ### Instruction: The following is a conversation between an AI and a human. The AI is going to say something to the human. Write what the AI is going to say
 in response to the human. The AI should respond in a way that is appropriate to the human's statement, and encourages the conversation
-to continue with follow up questions or statements.
+to continue with follow up questions or statements
 
-### Response:
-User: {input_prompt}
+### Response:{}
+
 AI: """
 
-def respond_to(input_text):
-    # Update system prompt with the user's input
-    prompt = system_prompt.format(input_prompt=input_text)
-    response = llm(prompt)
-    return response
+def update_history(history, role, text):
+    # Add the new interaction to the history
+    history.append((role, text))
+    # Format the history into a string
+    history_str = "\n".join(f"{role}: {text}" for role, text in history)
+    return history, history_str
 
-def main():
+def respond_to(input_text, history):
+    # Update the history with the user's input
+    history, history_str = update_history(history, "User", input_text)
+    # Update system prompt with the history and user's input
+    prompt = system_prompt.format(history_str)
+    # Initialize the response
+    response = ""
+    # Generate the AI's response
+    print("AI: ", end='', flush=True)  # Print the "AI: " prefix
+    for token in llm(prompt):
+        response += token
+        print(token, end='', flush=True)
+    print()  # print a newline at the end of the response
+    # Update the history with the AI's response
+    history, _ = update_history(history, "AI", response)
+    return history
+
+
+def main(history):
     while True:
         user_input = input("User: ")
-        response = respond_to(user_input)
-        print(f"AI: {response}")
+        history = respond_to(user_input, history)
 
 if __name__ == '__main__':
-    main()
+    history = main(history)
